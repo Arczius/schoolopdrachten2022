417a98180497bc8028ab636a54091d556240a68f
Arczius <70692175+Arczius@users.noreply.github.com>
2022-07-03 18:29:28 +0200

    updated base php

---
+++ backend/index.php
@@ -0,0 +1,12 @@
+<?php
+    include_once './config/config.php';
+    include_once './parts/head.php';
+?>
+
+<body>
+     
+    <?php
+        include_once './parts/footer.php';
+    ?>
+</body>
+</html>
